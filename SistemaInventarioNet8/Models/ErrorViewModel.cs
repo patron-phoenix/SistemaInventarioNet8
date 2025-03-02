7011f65dc2e4feb2f80b963c255d2d8c9a1d8c43
Ivan S <patron.phoenix@gmail.com>
2025-03-02 16:37:59 -0400

    Creacion del proyecto

---
+++ SistemaInventarioNet8/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaInventarioNet8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
