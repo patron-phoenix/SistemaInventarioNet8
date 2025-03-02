3538184c8d0a07ee5fc4981338281b04e7bdf560
Ivan S <patron.phoenix@gmail.com>
2025-03-02 18:42:14 -0400

    PATRON REPOSITORIO

---
+++ SistemaInventarioNet8.AccesoDatos/Repositorio/BodegaRepositorio.cs
@@ -0,0 +1,32 @@
+﻿using SistemaInventarioNet8.AccesoDatos.Data;
+using SistemaInventarioNet8.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventarioNet8.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventarioNet8.AccesoDatos.Repositorio
+{
+    public class BodegaRepositorio : Repositorio<Bodega>, IBodegaRepositorio
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BodegaRepositorio(ApplicationDbContext db):base(db)
+        {
+            _db = db;
+        }
+        public void Actualizar(Bodega bodega)
+        {
+            var bodegaDB = _db.Bodegas.FirstOrDefault(b => b.Id == bodega.Id);
+            if (bodegaDB != null)
+            {
+                bodegaDB.Nombre = bodega.Nombre;
+                bodegaDB.Descripcion = bodega.Descripcion;
+                bodegaDB.Estado=bodega.Estado;
+                _db.SaveChanges();
+            }
+        }
+    }
+}
